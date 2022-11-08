ddb916b3105f3b2cd79efa2327f901f610221f5e
a1tsv <forakk01@yandex.ru>
2022-11-08 13:53:26 +0300

    Updates

---
+++ precache-manifest.e132aa2e2356ae8374ce6d0ab5c9af9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14b151693c6a338093931916939e16b",
+    "url": "/ihw/index.html"
+  },
+  {
+    "revision": "2a78ff753bba2bab19b2",
+    "url": "/ihw/static/css/main.f24a29a6.chunk.css"
+  },
+  {
+    "revision": "92faccda99ac8a3f68f1",
+    "url": "/ihw/static/js/2.6f3c9da1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ihw/static/js/2.6f3c9da1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a78ff753bba2bab19b2",
+    "url": "/ihw/static/js/main.cb17968f.chunk.js"
+  },
+  {
+    "revision": "831172494c8f260d0b2e",
+    "url": "/ihw/static/js/runtime-main.ae74ff72.js"
+  }
+]);
